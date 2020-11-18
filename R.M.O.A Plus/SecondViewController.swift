35d5781043c0ecdddf0772d3a7a63d08346caf7a
Chung Le <chunglv@namvietmedia.com.vn>
2020-11-18 20:46:34 +0700

    push code

---
+++ R.M.O.A Plus/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  R.M.O.A Plus
+//
+//  Created by My iMac on 8/22/18.
+//  Copyright © 2018 My iMac. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # R.M.O.A
+
+* An application to manage rental apartment.
+* Using CoreData to save data.
